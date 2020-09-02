5586b004e5c6a8bfb8ae952044c6bd7ab5139c59
guillermo_ferraz <guillermoferrazdst@gmail.com>
2020-09-02 20:01:27 +0000

    selector de colores para notas 'ok', comenzando con comunidad Dev notes.

---
+++ src/models/NoteInfo.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const NoteInfoSchema = new Schema({
+	title: { type: String, required: true },
+	description: { type: String, required: true },
+	date: { type: Date, default: Date.now },
+	user: { type: String },
+
+},
+	
+	{
+		timestamps:true
+	});
+
+
+
+module.exports = mongoose.model('NoteInfo', NoteInfoSchema);
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const Note = require('../models/Note');
 const NoteDanger = require('../models/NoteDanger');
+const NoteWarning = require('../models/NoteWarning');
+const NoteInfo = require('../models/NoteInfo');
 const { isAuthenticated } = require('../helpers/auth')
 
 
@@ -21,10 +23,73 @@ router.post('/menu/notes/newNote', isAuthenticated, async (req,res) => {
 
 	res.redirect('/main/start');
 
-})
+});
+//Notes Color selector
+router.get('/main/newNoteC', (req, res) => {
+	res.render('menu/notes/selectColorNote');
+});
+//notes color danger
+router.get('/main/newNoteDanger', (req, res) => {
+	res.render('menu/notes/newNoteDanger');
+});
+//notes color warning
+router.get('/main/newNoteWarning', (req, res) => {
+	res.render('menu/notes/newNoteWarning');
+});
+//notes color info
+router.get('/main/newNoteInfo', (req, res) => {
+	res.render('menu/notes/newNoteInfo');
+});	
+
+//post note danger
+router.post('/main/newNoteDanger', isAuthenticated, async (req,res) => {
+	const { title, description } = req.body;
+	//console.log(req.body);
+
+	const newNoteDanger = new NoteDanger({title, description});
+	newNoteDanger.user = req.user.id;
+	console.log(newNoteDanger);
+	await newNoteDanger.save();
+	req.flash('success_msg', 'Nota agregada con exito.');
+
+
+	res.redirect('/main/start');
+});
+//post note warning
+router.post('/main/newNoteWarning', isAuthenticated, async (req,res) => {
+	const { title, description } = req.body;
+	//console.log(req.body);
+
+	const newNoteWarning = new NoteWarning({title, description});
+	newNoteWarning.user = req.user.id;
+	console.log(newNoteWarning);
+	await newNoteWarning.save();
+	req.flash('success_msg', 'Nota agregada con exito.');
+
+
+	res.redirect('/main/start');
+});
+//post note info
+router.post('/main/newNoteInfo', isAuthenticated, async (req,res) => {
+	const { title, description } = req.body;
+	//console.log(req.body);
+
+	const newNoteInfo = new NoteInfo({title, description});
+	newNoteInfo.user = req.user.id;
+	console.log(newNoteInfo);
+	await newNoteInfo.save();
+	req.flash('success_msg', 'Nota agregada con exito.');
+
+
+	res.redirect('/main/start');
+});		
+//
 router.get('/main/start', isAuthenticated, async (req, res) => {
 	const notes = await Note.find({user: req.user.id}).sort({date: 'desc'});
-	res.render('menu/notes/allNotes', { notes });
+	const notesDanger = await NoteDanger.find({user: req.user.id}).sort({date: 'desc'});
+	const notesWarning = await NoteWarning.find({user: req.user.id}).sort({date: 'desc'});
+	const notesInfo = await NoteInfo.find({user: req.user.id}).sort({date: 'desc'});
+	res.render('menu/notes/allNotes', { notes, notesDanger, notesWarning, notesInfo });
 });
 router.delete('/menu/notes/delete/:id', isAuthenticated, async(req, res) => {
 	await Note.findByIdAndDelete(req.params.id);
@@ -43,8 +108,65 @@ router.put('/main/editNote/:id', isAuthenticated, async (req, res) => {
 	res.redirect('/main/start');
 
 });
-//Notes Color danger
+//Edit note Danger.
+router.get('/main/editNoteDanger/:id', isAuthenticated, async (req, res) => {
+	const noteDanger = await NoteDanger.findById(req.params.id);
+	res.render('menu/notes/editNoteDanger', { noteDanger });
+});
 
+router.put('/main/editNoteDanger/:id', isAuthenticated, async (req, res) => {
+	const { title, description } = req.body;
+	await NoteDanger.findByIdAndUpdate(req.params.id, { title, description });
+	req.flash('success_msg', 'Nota actualizada con exito.');
+	res.redirect('/main/start');
 
+});
+//delete note Danger
+
+router.delete('/menu/notes/deleteDanger/:id', isAuthenticated, async(req, res) => {
+	await NoteDanger.findByIdAndDelete(req.params.id);
+	req.flash('success_msg', 'Nota eliminada con exito');
+	res.redirect('/main/start');
+});
+//edit note warning
+router.get('/main/editNoteWarning/:id', isAuthenticated, async (req, res) => {
+	const noteWarning = await NoteWarning.findById(req.params.id);
+	res.render('menu/notes/editNoteWarning', { noteWarning });
+});
+
+router.put('/main/editNoteWarning/:id', isAuthenticated, async (req, res) => {
+	const { title, description } = req.body;
+	await NoteWarning.findByIdAndUpdate(req.params.id, { title, description });
+	req.flash('success_msg', 'Nota actualizada con exito.');
+	res.redirect('/main/start');
+
+});
+//delete note warning
+
+router.delete('/menu/notes/deleteWarning/:id', isAuthenticated, async(req, res) => {
+	await NoteWarning.findByIdAndDelete(req.params.id);
+	req.flash('success_msg', 'Nota eliminada con exito');
+	res.redirect('/main/start');
+});
+
+//Edit note info.
+router.get('/main/editNoteInfo/:id', isAuthenticated, async (req, res) => {
+	const noteInfo = await NoteInfo.findById(req.params.id);
+	res.render('menu/notes/editNoteInfo', { noteInfo });
+});
+
+router.put('/main/editNoteInfo/:id', isAuthenticated, async (req, res) => {
+	const { title, description } = req.body;
+	await NoteInfo.findByIdAndUpdate(req.params.id, { title, description });
+	req.flash('success_msg', 'Nota actualizada con exito.');
+	res.redirect('/main/start');
+});
+//delete note info
+
+router.delete('/menu/notes/deleteInfo/:id', isAuthenticated, async(req, res) => {
+	await NoteInfo.findByIdAndDelete(req.params.id);
+	req.flash('success_msg', 'Nota eliminada con exito');
+	res.redirect('/main/start');
+});
 
 module.exports = router;
